5c5bc5de82baa8ea62eb1b7b3e4ea892c8fb2394
IvanSafonov1 <i.safonov@infinnitysolutions.com>
2019-06-05 08:29:14 +0500

    ФизЛица

---
+++ Education/modules/global/src/com/company/education/entity/Physical_person.java
@@ -0,0 +1,12 @@
+package com.company.education.entity;
+
+import com.haulmont.cuba.core.entity.StandardEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Table(name = "EDUCATION_PHYSICAL_PERSON")
+@Entity(name = "education_Physical_person")
+public class Physical_person extends StandardEntity {
+    private static final long serialVersionUID = -3127182255724693579L;
+}
