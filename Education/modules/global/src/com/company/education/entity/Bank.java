76280b0a0cf53ac138bfdf50bde6ed3ca7770d31
IvanSafonov1 <i.safonov@infinnitysolutions.com>
2019-06-05 08:21:15 +0500

    Банк

---
+++ Education/modules/global/src/com/company/education/entity/Bank.java
@@ -0,0 +1,38 @@
+package com.company.education.entity;
+
+import com.haulmont.cuba.core.entity.StandardEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Table(name = "EDUCATION_BANK")
+@Entity(name = "education_Bank")
+public class Bank extends StandardEntity {
+    private static final long serialVersionUID = 6886510743098474257L;
+
+    @NotNull
+    @Column(name = "NAME", nullable = false, unique = true)
+    protected String name;
+
+    @NotNull
+    @Column(name = "BIK", nullable = false, unique = true, length = 9)
+    protected String bik;
+
+    public String getBik() {
+        return bik;
+    }
+
+    public void setBik(String bik) {
+        this.bik = bik;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
