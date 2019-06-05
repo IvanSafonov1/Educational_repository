c5478bcddf2c88cd30233ed88754dcfc3bb823aa
IvanSafonov1 <i.safonov@infinnitysolutions.com>
2019-06-05 08:35:51 +0500

    ЮрЛица

---
+++ Education/modules/global/src/com/company/education/entity/Legal_person.java
@@ -0,0 +1,60 @@
+package com.company.education.entity;
+
+import com.haulmont.cuba.core.entity.StandardEntity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Table(name = "EDUCATION_LEGAL_PERSON")
+@Entity(name = "education_Legal_person")
+public class Legal_person extends StandardEntity {
+    private static final long serialVersionUID = -255844484957261276L;
+
+    @NotNull
+    @Column(name = "NAME_OF_COMPANY", nullable = false, unique = true)
+    protected String name_of_company;
+
+    @NotNull
+    @Column(name = "INN", nullable = false, unique = true, length = 12)
+    protected String inn;
+
+    @NotNull
+    @Column(name = "PERSONAL_ACCOUNT", nullable = false, unique = true, length = 10)
+    protected String personal_account;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "BANK_ID")
+    protected Bank bank;
+
+    public Bank getBank() {
+        return bank;
+    }
+
+    public void setBank(Bank bank) {
+        this.bank = bank;
+    }
+
+    public String getPersonal_account() {
+        return personal_account;
+    }
+
+    public void setPersonal_account(String personal_account) {
+        this.personal_account = personal_account;
+    }
+
+    public String getInn() {
+        return inn;
+    }
+
+    public void setInn(String inn) {
+        this.inn = inn;
+    }
+
+    public String getName_of_company() {
+        return name_of_company;
+    }
+
+    public void setName_of_company(String name_of_company) {
+        this.name_of_company = name_of_company;
+    }
+}
--- Education/modules/global/src/com/company/education/entity/Physical_person.java
+++ Education/modules/global/src/com/company/education/entity/Physical_person.java
@@ -2,11 +2,73 @@ package com.company.education.entity;
 
 import com.haulmont.cuba.core.entity.StandardEntity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 @Table(name = "EDUCATION_PHYSICAL_PERSON")
 @Entity(name = "education_Physical_person")
 public class Physical_person extends StandardEntity {
     private static final long serialVersionUID = -3127182255724693579L;
+
+    @NotNull
+    @Column(name = "NAME", nullable = false)
+    protected String name;
+
+    @NotNull
+    @Column(name = "LAST_NAME", nullable = false)
+    protected String last_name;
+
+    @NotNull
+    @Column(name = "MIDDLE_NAME", nullable = false)
+    protected String middle_name;
+
+    @NotNull
+    @Column(name = "PERSONAL_ACCOUNT", nullable = false, unique = true, length = 10)
+    protected String personal_account;
+
+    @NotNull
+    @Column(name = "AGE", nullable = false, length = 3)
+    protected String age;
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getPersonal_account() {
+        return personal_account;
+    }
+
+    public void setPersonal_account(String personal_account) {
+        this.personal_account = personal_account;
+    }
+
+    public String getMiddle_name() {
+        return middle_name;
+    }
+
+    public void setMiddle_name(String middle_name) {
+        this.middle_name = middle_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
