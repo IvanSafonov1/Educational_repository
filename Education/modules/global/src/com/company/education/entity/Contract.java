bea63d2476847bdcf0bf7584a2414055f5cea4a6
IvanSafonov1 <i.safonov@infinnitysolutions.com>
2019-06-05 09:09:54 +0500

    Договор

---
+++ Education/modules/global/src/com/company/education/entity/Contract.java
@@ -0,0 +1,12 @@
+package com.company.education.entity;
+
+import com.haulmont.cuba.core.entity.StandardEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Table(name = "EDUCATION_CONTRACT")
+@Entity(name = "education_Contract")
+public class Contract extends StandardEntity {
+    private static final long serialVersionUID = 7753655191035057149L;
+}
--- Education/modules/global/src/com/company/education/entity/Customer.java
+++ Education/modules/global/src/com/company/education/entity/Customer.java
@@ -2,11 +2,25 @@ package com.company.education.entity;
 
 import com.haulmont.cuba.core.entity.StandardEntity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 @Table(name = "EDUCATION_CUSTOMER")
 @Entity(name = "education_Customer")
 public class Customer extends StandardEntity {
     private static final long serialVersionUID = 2526165953448832759L;
+
+    @NotNull
+    @Column(name = "PERSONAL_ACCOUNT", nullable = false, unique = true, length = 10)
+    protected String personal_account;
+
+    public String getPersonal_account() {
+        return personal_account;
+    }
+
+    public void setPersonal_account(String personal_account) {
+        this.personal_account = personal_account;
+    }
 }
